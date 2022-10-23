25fa23453e24cc8a9773d736f58533b28f95555d
Ratmir Ishgulov <ishgulov.ratmir@gmail.com>
2022-10-23 15:43:36 +0300

    #1 add models in project

---
+++ Cube2d/Cube2d/Models/Graphics.cs
@@ -0,0 +1,61 @@
+﻿namespace Cube2d.Models
+{
+    public class Graphics
+    {
+        //ширина консоли
+        public int width { get; }
+        //Высота консоли
+        public int height { get; }
+        //соотношение высотаы и ширины
+        public float aspect { get; }
+        //соотношение высоты и ширины символа
+        public float pixelAspect { get; }
+        //множитель (фокусное расстояние) 
+        public float multiplier { get; }
+        //Массив символов
+        public char[] buffer { get; private set; }
+
+        public Graphics(int width, int height, float multiplier)
+        {
+            this.width = width;
+            this.height = height;
+            this.multiplier = multiplier;
+            aspect = (float)width / height;
+            pixelAspect = 8.0f / 12.0f;
+            buffer = new char[width * height];
+        }
+
+        //Чистим (заполняем буффер пробелами)
+        private void Reset()
+        {
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    buffer[i + j * width] = ' ';
+        }
+
+        public void Begin() // обнуляет буффер и положение курсора в консоли
+        {
+            Reset();
+            Console.SetCursorPosition(0, 0);
+        }
+
+        public void Draw(Shape shape) // Добавляет форму в буффер
+        {
+            // дров у шейп должно возвращать изменёный массив чаров где добавилась форма
+            char[] shapeBuffer = shape.Draw(this);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (shapeBuffer[i + j * width] != 0)
+                        buffer[i + j * width] = shapeBuffer[i + j * width];
+                }
+            }
+        }
+
+        public void WriteEnd() // Выводит буфер в консоль
+        {
+            Console.WriteLine(buffer);
+        }
+    }
+}
---
+++ Cube2d/Cube2d/Models/Vector2.cs
@@ -0,0 +1,32 @@
+﻿namespace Cube2d.Models
+{
+    public class Vector2
+    {
+        public float x { get; private set; }
+
+        public float y { get; private set; }
+
+        public Vector2()
+        {
+            //Для нулевого вектора
+            x = 0;
+            y = 0;
+        }
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2 { x = a.x + b.x, y = a.y + b.y };
+        }
+
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2 { y = a.x - b.x, x = a.y - b.y };
+        }
+    }
+}
